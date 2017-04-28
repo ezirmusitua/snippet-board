f2c3ae917363d7447d33624af807d60fbc22cb1a
ezirmusitua <jferroal@gmail.com>
2017-04-28 22:54:15 +0800

    Remove unnecessary

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,6 @@ def create_app(config_name):
     app = Flask(__name__)
     current_config = config[config_name]
     app.config.from_object(current_config)
-    print current_config
     current_config.init_app(app)
 
     db.init_app(app)
--- config.py
+++ config.py
@@ -5,8 +5,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-print 'sqlite:///' + os.path.join(basedir, 'db/snippet-.sqlite')
-
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get(
